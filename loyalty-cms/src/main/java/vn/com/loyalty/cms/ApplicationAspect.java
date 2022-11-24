0f347607705658d393ff3444ad7b15c0cbc1a4c7
toanbv <toanbv1997@gmail.com>
2022-11-24 17:48:53 +0700

    update

--- loyalty-cms/src/main/java/vn/com/loyalty/cms/ApplicationAspect.java
+++ loyalty-cms/src/main/java/vn/com/loyalty/cms/ApplicationAspect.java
@@ -5,11 +5,31 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.*;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Aspect
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class ApplicationAspect {
 
+
+    synchronized void a() {
+
+        // get str từ db
+        // xử lý
+        // update log
+        // update data db db
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+        ExecutorService service = Executors.newFixedThreadPool(1);
+
+    }
 
 }
--- script/build.sh
+++ script/build.sh
@@ -12,3 +12,12 @@ docker build -f Dockerfile.voucher -t loyalty-voucher:1.1 .
 docker-compose -f docker-compose-local.yml up
 
 
+#!/bin/bash
+# Use this for user data (script from top to bottom)
+# install httpd (linux 2 version)
+
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Hello world from ${hostname -f}</h1>" > /var/www/html/index.html
