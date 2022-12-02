210046d72746291a15ac6193e7f8f38adcdba30d
toanbv <toanbv1997@gmail.com>
2022-12-02 17:55:27 +0700

    refactor

--- loyalty-transaction/src/main/java/vn/com/loyalty/transaction/LoyaltyTransactionApplication.java
+++ loyalty-transaction/src/main/java/vn/com/loyalty/transaction/LoyaltyTransactionApplication.java
@@ -1,9 +1,13 @@
 package vn.com.loyalty.transaction;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,6 +18,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ComponentScan("vn.com.loyalty")
 @EnableJpaRepositories("vn.com.loyalty.core.repository")
 @EntityScan("vn.com.loyalty.core.entity")
+@PropertySource("classpath:application.yaml")
 public class LoyaltyTransactionApplication {
 
     public static void main(String[] args) {
